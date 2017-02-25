b9236c4b4c37c171b705d9defe1737a8be7664bb
schmitzl <lisa.schmitz66@googlemail.com>
2017-02-25 13:21:03 +0100

    copy function code for loading head model into code flow

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ app.updateEvent.addEventListener(function (frame) {
     infoText += 'Your location is lla (' + toFixed(gpsCartographicDeg[0], 6) + ', ';
     infoText += toFixed(gpsCartographicDeg[1], 6) + ', ' + toFixed(gpsCartographicDeg[2], 2) + ')';
     infoText += 'box is ' + toFixed(distanceToBox, 2) + ' meters away'; */
-    var boxLabelText = 'a wooden box!<br>lla = ' + toFixed(boxCartographicDeg[0], 6) + ', ';
+    var boxLabelText = 'wooden box!<br>lla = ' + toFixed(boxCartographicDeg[0], 6) + ', ';
     boxLabelText += toFixed(boxCartographicDeg[1], 6) + ', ' + toFixed(boxCartographicDeg[2], 2);
   /*  if (lastInfoText !== infoText) {
         locationElements[0].innerHTML = infoText;
