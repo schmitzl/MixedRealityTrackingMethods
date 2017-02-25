9928ce409130d2611a445828248230f42df3d657
schmitzl <lisa.schmitz66@googlemail.com>
2017-02-25 13:28:27 +0100

    added missing declaration of textureLoader

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
 // for the user's location
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera();
-var userLocation = new THREE.Object3D;
+var userLocation = new THREE.Object3D();
 scene.add(camera);
 scene.add(userLocation);
 // The CSS3DArgonRenderer supports mono and stereo views.  Currently
@@ -65,24 +65,26 @@ app.view.element.appendChild(cssRenderer.domElement);
 
 // -- INSERT LOADING HEAD MODEL --
 var headModel = new THREE.Object3D();
+var mesh;
+var textureLoader = new THREE.TextureLoader();
 //loadLeePerrySmith();
 
- var loader = new THREE.JSONLoader();
-    loader.load('resources/obj/leeperrysmith/LeePerrySmith.js', function (geometry) {
-        var material = new THREE.MeshPhongMaterial({
-            specular: 0x111111,
-            map: textureLoader.load('resources/obj/leeperrysmith/Map-COL.jpg'),
-            specularMap: textureLoader.load('resources/obj/leeperrysmith/Map-SPEC.jpg'),
-            normalMap: textureLoader.load('resources/obj/leeperrysmith/Infinite-Level_02_Tangent_SmoothUV.jpg'),
-            normalScale: new THREE.Vector2(0.75, 0.75),
-            shininess: 25
-        });
-        mesh = new THREE.Mesh(geometry, material);
-        // add the model to the headModel object, not the scene
-        headModel.add(mesh);
-        mesh.scale.set(.02, .02, .02);
-        mesh.rotation.x = THREE.Math.degToRad(90);
+var loader = new THREE.JSONLoader();
+loader.load('resources/obj/leeperrysmith/LeePerrySmith.js', function (geometry) {
+    var material = new THREE.MeshPhongMaterial({
+        specular: 0x111111,
+        map: textureLoader.load('resources/obj/leeperrysmith/Map-COL.jpg'),
+        specularMap: textureLoader.load('resources/obj/leeperrysmith/Map-SPEC.jpg'),
+        normalMap: textureLoader.load('resources/obj/leeperrysmith/Infinite-Level_02_Tangent_SmoothUV.jpg'),
+        normalScale: new THREE.Vector2(0.75, 0.75),
+        shininess: 25
     });
+    mesh = new THREE.Mesh(geometry, material);
+    // add the model to the headModel object, not the scene
+    headModel.add(mesh);
+    mesh.scale.set(.02, .02, .02);
+    mesh.rotation.x = THREE.Math.degToRad(90);
+});
 
 
 
