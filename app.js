99d9177e7c61aaaa26098acb66e4ac10652647f3
Hlynsson <hlynurd@gmail.com>
2017-02-27 20:07:03 +0100

    Layout changes

--- app.js
+++ app.js
@@ -36,10 +36,12 @@ app.view.element.appendChild(hud.domElement);
 // the CSS3DArgonHUD hudElement.
 var hudContent = document.getElementById('hud');
 hud.appendChild(hudContent);
+var arrowContainer = document.getElementById('arrowContainer');
+hudContent.appendChild(arrowContainer);
 var arrow = document.getElementById('arrow');
 //  We also move the description box to the Argon HUD, but moving it inside the 'hud' element
 //var hudDescription = document.getElementById('description');
-hudContent.appendChild(arrow);
+arrowContainer.appendChild(arrow);
 // All geospatial objects need to have an Object3D linked to a Cesium Entity.
 // We need to do this because Argon needs a mapping between Entities and Object3Ds.
 
