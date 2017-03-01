24cc7285f9086da0dad47c019c9c43c0624fb920
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-01 14:01:29 +0100

    displaying tram model with Vuforia

--- app.js
+++ app.js
@@ -2,113 +2,49 @@
 /// <reference types="three" />
 /// <reference types="dat-gui" />
 /// <reference types="stats" />
+var Cesium = Argon.Cesium;
+var Cartesian3 = Argon.Cesium.Cartesian3;
+var ReferenceFrame = Argon.Cesium.ReferenceFrame;
+var JulianDate = Argon.Cesium.JulianDate;
+var CesiumMath = Argon.Cesium.CesiumMath;
+
 // set up Argon
 var app = Argon.init();
-// set up THREE.  Create a scene, a perspective camera and an object
-// for the user's location
+// set up THREE.  
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera();
 var userLocation = new THREE.Object3D();
 scene.add(camera);
 scene.add(userLocation);
 scene.autoUpdate = false;
-// We use the standard WebGLRenderer when we only need WebGL-based content
+
+// set up renderer
+var cssRenderer = new THREE.CSS3DArgonRenderer();
+var hud = new THREE.CSS3DArgonHUD(); // place for fixed html screen content
 var renderer = new THREE.WebGLRenderer({
     alpha: true,
     logarithmicDepthBuffer: true
 });
-// account for the pixel density of the device
 renderer.setPixelRatio(window.devicePixelRatio);
 app.view.element.appendChild(renderer.domElement);
-// to easily control stuff on the display
-var hud = new THREE.CSS3DArgonHUD();
-// We put some elements in the index.html, for convenience. 
-// Here, we retrieve the description box and move it to the 
-// the CSS3DArgonHUD hudElements[0].  We only put it in the left
-// hud since we'll be hiding it in stereo
-var description = document.getElementById('description');
-hud.hudElements[0].appendChild(description);
+app.view.element.appendChild(cssRenderer.domElement);
 app.view.element.appendChild(hud.domElement);
+
+
 // let's show the rendering stats
 var stats = new Stats();
 hud.hudElements[0].appendChild(stats.dom);
-// Tell argon what local coordinate system you want.  The default coordinate
-// frame used by Argon is Cesium's FIXED frame, which is centered at the center
-// of the earth and oriented with the earth's axes.  
-// The FIXED frame is inconvenient for a number of reasons: the numbers used are
-// large and cause issues with rendering, and the orientation of the user's "local
-// view of the world" is different that the FIXED orientation (my perception of "up"
-// does not correspond to one of the FIXED axes).  
-// Therefore, Argon uses a local coordinate frame that sits on a plane tangent to 
-// the earth near the user's current location.  This frame automatically changes if the
-// user moves more than a few kilometers.
-// The EUS frame cooresponds to the typical 3D computer graphics coordinate frame, so we use
-// that here.  The other option Argon supports is localOriginEastNorthUp, which is
-// more similar to what is used in the geospatial industry
+
 app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
-// create a bit of animated 3D text that says "argon.js" to display 
-var uniforms = {
-    amplitude: { type: "f", value: 0.0 }
-};
-var argonTextObject = new THREE.Object3D();
-argonTextObject.position.z = -0.5;
-userLocation.add(argonTextObject);
-var loader = new THREE.FontLoader();
-loader.load('resources/fonts/helvetiker_bold.typeface.js', function (font) {
-    var textGeometry = new THREE.TextGeometry("argon.js", {
-        font: font,
-        size: 40,
-        height: 5,
-        curveSegments: 3,
-        bevelThickness: 2,
-        bevelSize: 1,
-        bevelEnabled: true
-    });
-    textGeometry.center();
-    var tessellateModifier = new THREE.TessellateModifier(8);
-    for (var i = 0; i < 6; i++) {
-        tessellateModifier.modify(textGeometry);
-    }
-    var explodeModifier = new THREE.ExplodeModifier();
-    explodeModifier.modify(textGeometry);
-    var numFaces = textGeometry.faces.length;
-    var bufferGeometry = new THREE.BufferGeometry().fromGeometry(textGeometry);
-    var colors = new Float32Array(numFaces * 3 * 3);
-    var displacement = new Float32Array(numFaces * 3 * 3);
-    var color = new THREE.Color();
-    for (var f = 0; f < numFaces; f++) {
-        var index = 9 * f;
-        var h = 0.07 + 0.1 * Math.random();
-        var s = 0.5 + 0.5 * Math.random();
-        var l = 0.6 + 0.4 * Math.random();
-        color.setHSL(h, s, l);
-        var d = 5 + 20 * (0.5 - Math.random());
-        for (var i = 0; i < 3; i++) {
-            colors[index + (3 * i)] = color.r;
-            colors[index + (3 * i) + 1] = color.g;
-            colors[index + (3 * i) + 2] = color.b;
-            displacement[index + (3 * i)] = d;
-            displacement[index + (3 * i) + 1] = d;
-            displacement[index + (3 * i) + 2] = d;
-        }
-    }
-    bufferGeometry.addAttribute('customColor', new THREE.BufferAttribute(colors, 3));
-    bufferGeometry.addAttribute('displacement', new THREE.BufferAttribute(displacement, 3));
-    var shaderMaterial = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: "\n            uniform float amplitude;\n            attribute vec3 customColor;\n            attribute vec3 displacement;\n            varying vec3 vNormal;\n            varying vec3 vColor;\n            void main() {\n                vNormal = normal;\n                vColor = customColor;\n                vec3 newPosition = position + normal * amplitude * displacement;\n                gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );\n            }\n        ",
-        fragmentShader: "\n            varying vec3 vNormal;\n            varying vec3 vColor;\n            void main() {\n                const float ambient = 0.4;\n                vec3 light = vec3( 1.0 );\n                light = normalize( light );\n                float directional = max( dot( vNormal, light ), 0.0 );\n                gl_FragColor = vec4( ( directional + ambient ) * vColor, 1.0 );\n            }\n        "
-    });
-    var textMesh = new THREE.Mesh(bufferGeometry, shaderMaterial);
-    argonTextObject.add(textMesh);
-    argonTextObject.scale.set(0.001, 0.001, 0.001);
-    argonTextObject.position.z = -0.50;
-    // add an argon updateEvent listener to slowly change the text over time.
-    // we don't have to pack all our logic into one listener.
-    app.context.updateEvent.addEventListener(function () {
-        uniforms.amplitude.value = 1.0 + Math.sin(Date.now() * 0.001 * 0.5);
-    });
-});
+
+// -- LOAD GEO TRAM --
+// load tram model
+var tramModel = new THREE.Object3D();
+var frameModel = new THREE.Object3D();
+loadTram();
+
+
+// connect to Vuforia
 app.vuforia.isAvailable().then(function (available) {
     // vuforia not available on this platform
     if (!available) {
@@ -134,34 +70,34 @@ app.vuforia.isAvailable().then(function (available) {
                 // coordinate frame relative to the camera.  Because they are Cesium
                 // entities, we can ask for their pose in any coordinate frame we know
                 // about.
-                var gvuBrochureEntity = app.context.subscribeToEntityById(trackables["tram"].id);
+                var tramMarkerEntity = app.context.subscribeToEntityById(trackables["tram"].id);
                 // create a THREE object to put on the trackable
-                var gvuBrochureObject = new THREE.Object3D;
-                scene.add(gvuBrochureObject);
+                var tramMarkerObject = new THREE.Object3D;
+                scene.add(tramMarkerObject);
                 // the updateEvent is called each time the 3D world should be
                 // rendered, before the renderEvent.  The state of your application
                 // should be updated here.
                 app.context.updateEvent.addEventListener(function () {
-                    // get the pose (in local coordinates) of the gvuBrochure target
-                    var gvuBrochurePose = app.context.getEntityPose(gvuBrochureEntity);
+                    // get the pose (in local coordinates) of the tramMarker target
+                    var tramMarkerPose = app.context.getEntityPose(tramMarkerEntity);
                     // if the pose is known the target is visible, so set the
                     // THREE object to the location and orientation
-                    if (gvuBrochurePose.poseStatus & Argon.PoseStatus.KNOWN) {
-                        gvuBrochureObject.position.copy(gvuBrochurePose.position);
-                        gvuBrochureObject.quaternion.copy(gvuBrochurePose.orientation);
+                    if (tramMarkerPose.poseStatus & Argon.PoseStatus.KNOWN) {
+                        tramMarkerObject.position.copy(tramMarkerPose.position);
+                        tramMarkerObject.quaternion.copy(tramMarkerPose.orientation);
                     }
                     // when the target is first seen after not being seen, the 
                     // status is FOUND.  Here, we move the 3D text object from the
                     // world to the target.
                     // when the target is first lost after being seen, the status 
                     // is LOST.  Here, we move the 3D text object back to the world
-                    if (gvuBrochurePose.poseStatus & Argon.PoseStatus.FOUND) {
-                        gvuBrochureObject.add(argonTextObject);
+                    if (tramMarkerPose.poseStatus & Argon.PoseStatus.FOUND) {
+                        tramMarkerObject.add(tramModel);
                         argonTextObject.position.z = 0;
                     }
-                    else if (gvuBrochurePose.poseStatus & Argon.PoseStatus.LOST) {
+                    else if (tramMarkerPose.poseStatus & Argon.PoseStatus.LOST) {
                         argonTextObject.position.z = -0.50;
-                        userLocation.add(argonTextObject);
+                        userLocation.add(tramModel);
                     }
                 });
             })["catch"](function (err) {
@@ -228,3 +164,40 @@ app.renderEvent.addEventListener(function () {
     }
 });
 
+function loadTram() {
+    var tramMesh;
+    var tramTextureLoader = new THREE.TextureLoader();
+    var tramGeometry = new THREE.Geometry();
+    
+    var tramLoader = new THREE.JSONLoader();
+    tramLoader.load('resources/obj/tram/tram.js', function (tramGeometry) {
+        var tramMaterial = new THREE.MeshPhongMaterial({
+            specular: 0x111111,
+            map: tramTextureLoader.load('resources/obj/tram/b_tramBase_Albedo.png'),
+            specularMap: tramTextureLoader.load('resources/obj/tram/b_tramBase_Metallic.png'),
+            normalMap: tramTextureLoader.load('resources/obj/tram/b_tramBase_Normal.png'),
+            //normalScale: new THREE.Vector2(0.75, 0.75),
+            shininess: 25
+        });
+        tramMesh = new THREE.Mesh(tramGeometry, tramMaterial);
+        // add the model to the tramModel object, not the scene
+        tramModel.add(tramMesh);
+      //  mesh.scale.set(.4, .4, .4);
+       // mesh.rotation.x = THREE.Math.degToRad(90);
+    });
+    
+    
+    var frameMesh;
+    var frameTextureLoader = new THREE.TextureLoader();
+    var frameGeometry = new THREE.Geometry();
+
+     var frameLoader = new THREE.JSONLoader();
+     frameLoader.load('resources/obj/tram/frame.js', function (frameGeometry) {
+        var frameMaterial = new THREE.MeshLambertMaterial({color: 0x000000});
+        frameMesh = new THREE.Mesh(frameGeometry, frameMaterial);
+        // add the model to the tramModel object, not the scene
+        frameModel.add(frameMesh);
+      //  mesh.scale.set(.4, .4, .4);
+       // mesh.rotation.x = THREE.Math.degToRad(90);
+    });
+}
