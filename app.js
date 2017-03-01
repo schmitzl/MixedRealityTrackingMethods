4b4a060f34efbcc0c71926b5f64544e52212daaf
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-01 11:07:08 +0100

    testing argon vuforia example

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /// <reference types="@argonjs/argon" />
 /// <reference types="three" />
 // grab some handles on APIs we use
-var Cesium = Argon.Cesium;
+/*var Cesium = Argon.Cesium;
 var Cartesian3 = Argon.Cesium.Cartesian3;
 var ReferenceFrame = Argon.Cesium.ReferenceFrame;
 var JulianDate = Argon.Cesium.JulianDate;
@@ -70,7 +70,8 @@ arrow.scale.set(0.02,0.02,0.02);
 arrow.position.set(0,1.25,0); */
 
 
-// Create a DIV to use to label the position and distance of the tram
+// Create a DIV to use to label the position and distance of the tram 
+/*
 var tramLocDiv = document.getElementById("box-location");
 var tramLabel = new THREE.CSS3DSprite(tramLocDiv);
 tramLabel.scale.set(0.02, 0.02, 0.02);
@@ -78,13 +79,127 @@ tramLabel.position.set(0, 1.25, 0);
 tramGeoObject.add(tramLabel);
 var tramInit = false;
 var tramCartographicDeg = [0, 0, 0];
-var lastTramText = '';
+var lastTramText = ''; */
 
 
 
 
 // -- LOAD VUFORIA TRAM --
-
+/// <reference types="@argonjs/argon" />
+/// <reference types="three" />
+/// <reference types="dat-gui" />
+/// <reference types="stats" />
+// set up Argon
+var app = Argon.init();
+// set up THREE.  Create a scene, a perspective camera and an object
+// for the user's location
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var userLocation = new THREE.Object3D();
+scene.add(camera);
+scene.add(userLocation);
+// We use the standard WebGLRenderer when we only need WebGL-based content
+var renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    logarithmicDepthBuffer: true
+});
+// account for the pixel density of the device
+renderer.setPixelRatio(window.devicePixelRatio);
+app.view.element.appendChild(renderer.domElement);
+// to easily control stuff on the display
+var hud = new THREE.CSS3DArgonHUD();
+// We put some elements in the index.html, for convenience. 
+// Here, we retrieve the description box and move it to the 
+// the CSS3DArgonHUD hudElements[0].  We only put it in the left
+// hud since we'll be hiding it in stereo
+var description = document.getElementById('description');
+hud.hudElements[0].appendChild(description);
+app.view.element.appendChild(hud.domElement);
+// let's show the rendering stats
+var stats = new Stats();
+hud.hudElements[0].appendChild(stats.dom);
+// Tell argon what local coordinate system you want.  The default coordinate
+// frame used by Argon is Cesium's FIXED frame, which is centered at the center
+// of the earth and oriented with the earth's axes.  
+// The FIXED frame is inconvenient for a number of reasons: the numbers used are
+// large and cause issues with rendering, and the orientation of the user's "local
+// view of the world" is different that the FIXED orientation (my perception of "up"
+// does not correspond to one of the FIXED axes).  
+// Therefore, Argon uses a local coordinate frame that sits on a plane tangent to 
+// the earth near the user's current location.  This frame automatically changes if the
+// user moves more than a few kilometers.
+// The EUS frame cooresponds to the typical 3D computer graphics coordinate frame, so we use
+// that here.  The other option Argon supports is localOriginEastNorthUp, which is
+// more similar to what is used in the geospatial industry
+app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+// create a bit of animated 3D text that says "argon.js" to display 
+var uniforms = {
+    amplitude: { type: "f", value: 0.0 }
+};
+var argonTextObject = new THREE.Object3D();
+argonTextObject.position.z = -0.50;
+userLocation.add(argonTextObject);
+var loader = new THREE.FontLoader();
+loader.load('../resources/fonts/helvetiker_bold.typeface.js', function (font) {
+    var textGeometry = new THREE.TextGeometry("argon.js", {
+        font: font,
+        size: 40,
+        height: 5,
+        curveSegments: 3,
+        bevelThickness: 2,
+        bevelSize: 1,
+        bevelEnabled: true
+    });
+    textGeometry.center();
+    var tessellateModifier = new THREE.TessellateModifier(8);
+    for (var i = 0; i < 6; i++) {
+        tessellateModifier.modify(textGeometry);
+    }
+    var explodeModifier = new THREE.ExplodeModifier();
+    explodeModifier.modify(textGeometry);
+    var numFaces = textGeometry.faces.length;
+    var bufferGeometry = new THREE.BufferGeometry().fromGeometry(textGeometry);
+    var colors = new Float32Array(numFaces * 3 * 3);
+    var displacement = new Float32Array(numFaces * 3 * 3);
+    var color = new THREE.Color();
+    for (var f = 0; f < numFaces; f++) {
+        var index = 9 * f;
+        var h = 0.07 + 0.1 * Math.random();
+        var s = 0.5 + 0.5 * Math.random();
+        var l = 0.6 + 0.4 * Math.random();
+        color.setHSL(h, s, l);
+        var d = 5 + 20 * (0.5 - Math.random());
+        for (var i = 0; i < 3; i++) {
+            colors[index + (3 * i)] = color.r;
+            colors[index + (3 * i) + 1] = color.g;
+            colors[index + (3 * i) + 2] = color.b;
+            displacement[index + (3 * i)] = d;
+            displacement[index + (3 * i) + 1] = d;
+            displacement[index + (3 * i) + 2] = d;
+        }
+    }
+    bufferGeometry.addAttribute('customColor', new THREE.BufferAttribute(colors, 3));
+    bufferGeometry.addAttribute('displacement', new THREE.BufferAttribute(displacement, 3));
+    var shaderMaterial = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: "\n            uniform float amplitude;\n            attribute vec3 customColor;\n            attribute vec3 displacement;\n            varying vec3 vNormal;\n            varying vec3 vColor;\n            void main() {\n                vNormal = normal;\n                vColor = customColor;\n                vec3 newPosition = position + normal * amplitude * displacement;\n                gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );\n            }\n        ",
+        fragmentShader: "\n            varying vec3 vNormal;\n            varying vec3 vColor;\n            void main() {\n                const float ambient = 0.4;\n                vec3 light = vec3( 1.0 );\n                light = normalize( light );\n                float directional = max( dot( vNormal, light ), 0.0 );\n                gl_FragColor = vec4( ( directional + ambient ) * vColor, 1.0 );\n            }\n        "
+    });
+    var textMesh = new THREE.Mesh(bufferGeometry, shaderMaterial);
+    argonTextObject.add(textMesh);
+    argonTextObject.scale.set(0.001, 0.001, 0.001);
+    // add an argon updateEvent listener to slowly change the text over time.
+    // we don't have to pack all our logic into one listener.
+    app.context.updateEvent.addEventListener(function () {
+        uniforms.amplitude.value = 1.0 + Math.sin(Date.now() * 0.001 * 0.5);
+    });
+});
+app.vuforia.isAvailable().then(function (available) {
+    // vuforia not available on this platform
+    if (!available) {
+        console.warn("vuforia not available on this platform.");
+        return;
+    }
 
     
 app.vuforia.isAvailable().then(function (available) {
@@ -136,12 +251,12 @@ api.objectTracker.createDataSet("resources/datasets/ArgonTutorial.xml").then(fun
             // when the target is first lost after being seen, the status 
             // is LOST.  Here, we move the 3D text object back to the world
             if (tramObjectPose.poseStatus & Argon.PoseStatus.FOUND) {
-                tramIconObject.add(tramModel);
+                tramIconObject.add(argonTextObject);
                 tramModel.position.z = 0;
             }
             else if (tramObjectPose.poseStatus & Argon.PoseStatus.LOST) {
                 tramModel.position.z = -0.5;
-                userLocation.add(tramModel);
+                userLocation.add(argonTextObject);
             } 
         });
     }).catch(function (err) {
