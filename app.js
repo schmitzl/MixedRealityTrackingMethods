9271bdb7e600165954bcd16879a96c61c0600c08
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-08 10:14:31 +0100

    debugging

--- app.js
+++ app.js
@@ -211,7 +211,7 @@ function loadTram() {
     var windowMesh;
     var windowTextureLoader = new THREE.TextureLoader();
     var windowGeometry = new THREE.Geometry();
-    var windowLoader = THREE.JSONLoader();
+    var windowLoader = new THREE.JSONLoader();
     /*windowLoader.load('resources/obj/tram/stoneportal.js', function (windowGeometry) {
         var windowMaterial = new THREE.MeshPhongMaterial({
             specular: 0x111111,
