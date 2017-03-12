d9cdf0c91d5b4946b333218492f51f957b25ab29
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-12 13:10:19 +0100

    working on tram movement

--- app.js
+++ app.js
@@ -76,8 +76,8 @@ tramScene.rotation.y = Math.PI;
 tramScene.translateX(-1);
 
 var speed = 0.0;
-var tramStartPositionX = tramBase.position.x;
-var tramStartPositionZ = tramBase.position.z;
+//var tramStartPositionX = tramBase.position.x;
+//var tramStartPositionZ = tramBase.position.z;
 
 alert(tramStartPositionZ);
 
