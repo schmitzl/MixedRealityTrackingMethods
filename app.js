6f77953d7fdc12fe47dbb89c977dcc19314a1447
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-16 15:25:57 +0100

    add flow of experiment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var ReferenceFrame = Argon.Cesium.ReferenceFrame;
 var JulianDate = Argon.Cesium.JulianDate;
 var CesiumMath = Argon.Cesium.CesiumMath;
 
+var order = {1,2,3};
 var i = 0;
 const GRAFFITI_NUM = 1;
 const PORTAL_NUM = 2;
