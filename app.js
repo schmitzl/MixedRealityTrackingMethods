b3e51a83a74b6e4a77bd9a60b9845da10d6bf352
schmitzl <lisa.schmitz66@googlemail.com>
2017-04-04 01:22:35 +0200

    added red cubes as obstacles for screenshot taking

--- app.js
+++ app.js
@@ -62,13 +62,17 @@ app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
 var box1Geometry = new THREE.BoxGeometry(1,3,1);
 var box1Material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var box1 = new THREE.Mesh(box1Geometry,box1Material);
+var box1Obj = new THREE.Object3D();
+box1Obj.add(box1);
 
 var box2Geometry = new THREE.BoxGeometry(1,3,1);
 var box2Material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var box2 = new THREE.Mesh(box2Geometry,box2Material);
+var box2Obj = new THREE.Object3D();
+box2Obj.add(box2);
 
-scene.add(box1);
-scene.add(box2);
+scene.add(box1Obj);
+scene.add(box2Obj);
 
 // -- LOAD SCENES --
 var tramScene = new THREE.Object3D();
@@ -135,7 +139,6 @@ app.vuforia.isAvailable().then(function (available) {
                         if ( tramMarkerPose.poseStatus & Argon.PoseStatus.KNOWN) {
                             tramMarkerObject.position.copy(tramMarkerPose.position);
                             tramMarkerObject.quaternion.copy(tramMarkerPose.orientation);
-                            box1.position.copy(tramMarkerPose.position);
                         }
                         if (tramMarkerPose.poseStatus & Argon.PoseStatus.FOUND) {
                             document.getElementById("heading").innerHTML = "Move the tram";
@@ -172,7 +175,7 @@ app.vuforia.isAvailable().then(function (available) {
                         }
                 } else if (step == graffiti_step) {
                     
-                       
+                           
                     
                         var graffitiMarkerPose = app.context.getEntityPose(graffitiMarkerEntity);
                         if ( graffitiMarkerPose.poseStatus & Argon.PoseStatus.KNOWN) {
@@ -197,7 +200,7 @@ app.vuforia.isAvailable().then(function (available) {
                                 document.getElementById("heading").innerHTML = "Take a screenshot";
                                 document.getElementById("instructions-graffiti-screenshot").style.display = "inline";
                                 isTakingScreenshot = true;
-                               
+                               box1Obj.position.copy(graffitiMarkerPose.position);
                               //  box1.position.copy(graffitiTramScene.position);
                                 //box2.position.copy(graffitiTramScene.position);
                             }
